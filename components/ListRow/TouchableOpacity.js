9e76f5d6ab7d3fafe596db0f2b35c56bdd11e7af
yz1311 <yz1311@gmail.com>
2021-03-16 10:08:40 +0800

    修复对0.64的兼容

--- components/ListRow/TouchableOpacity.js
+++ components/ListRow/TouchableOpacity.js
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import {Platform, TouchableWithoutFeedback, Animated, ViewPropTypes} from 'react-native';
-import Easing from 'react-native/Libraries/Animated/src/Easing';
 import flattenStyle from 'react-native/Libraries/StyleSheet/flattenStyle';
 
 if (Platform.constants.reactNativeVersion.major === 0 && Platform.constants.reactNativeVersion.minor < 62) {
@@ -127,6 +126,13 @@ export default class TouchableOpacity extends Component {
    * Animate the touchable to a new opacity.
    */
   _setOpacityTo(toValue, duration) {
+    let Easing;
+    try {
+      Easing = require('react-native/Libraries/Animated/src/Easing').default;
+    } catch (e) {
+      //RN0.64改变了Easing的路径
+      Easing = require('react-native/Libraries/Animated/Easing').default;
+    }
     Animated.timing(this.state.anim, {
       toValue,
       duration,
